4e5f644825571ee952e064161b0f69f8edd2b89c
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-12-09 11:17:09 +0330

    Day 03 | Part 2 Awesome!!!

--- 2024/day03/main.py
+++ 2024/day03/main.py
@@ -1,13 +1,19 @@
 from typing import List
 
 class Reader():
-    def __init__(self, content: str) -> None:
+    def __init__(self, content: str, respect_inst: bool) -> None:
         self.__content = content
+        self.__respect_inst = respect_inst
         self.__pointer = 0
         self.__enabled = True
 
     def __is_pointer_out_of_bound(self) -> bool:
        return self.__pointer >= len(self.__content)
+
+    def peek(self):
+        if self.__is_pointer_out_of_bound():
+            return None
+        return self.__content[self.__pointer]
 
     def read_char(self) -> str | None:
         if self.__is_pointer_out_of_bound():
@@ -30,7 +36,7 @@ class Reader():
             return False
         return True if self.__content[self.__pointer] == ch else False
 
-    def expect_peek(self, ch: str) -> bool:
+    def expect_read(self, ch: str) -> bool:
         exp = self.expect(ch)
         if exp:
             self.read_char()
@@ -55,7 +61,7 @@ class Reader():
 
     def expect_peek_seq(self, seq: str):
         for ch in seq:
-            exp = self.expect_peek(ch)
+            exp = self.expect_read(ch)
             if not exp:
                 return False
         return True
@@ -69,37 +75,50 @@ class Reader():
 
         starting_seq = self.read_until(["m", "d"])
         if starting_seq == "m":
-            read_seq = self.expect_peek_seq("ul(")
-            if not read_seq:
+            possible_mul_seq = self.expect_peek_seq("ul(")
+            if not possible_mul_seq:
                 return None
 
             f_int = self.read_int()
-            if not self.expect_peek(","):
+            if not self.expect_read(","):
                 return None
             s_int = self.read_int()
 
             if not f_int or not s_int:
                 return None
-            if not self.expect_peek(")"):
+            if not self.expect_read(")"):
                 return None
 
             if self.__enabled:
                 return f_int, s_int
             else:
                 return None
-        elif starting_seq == "d":
-            pass
+        elif starting_seq == "d" and self.__respect_inst:
+            if not self.expect_read("o"):
+                return None
+
+            peeked = self.peek()
+            if peeked == "n":
+                possible_neg_seq = self.expect_peek_seq("n't()")
+                if not possible_neg_seq:
+                    return None
+                self.__enabled = False
+            elif peeked == "(":
+                possible_pos_seq = self.expect_peek_seq("()")
+                if not possible_pos_seq:
+                    return None
+                self.__enabled = True
 
 
         return None
 
 
-def get_reader(filename = "input.txt"):
+def get_reader(filename = "input.txt", respect_inst = False):
     file = open(filename)
     content = file.read()
     file.close()
 
-    return Reader(content)
+    return Reader(content, respect_inst)
 
 def part_one():
     mem = get_reader()
@@ -110,5 +129,16 @@ def part_one():
             total_mul += f * s
     print(f"Part one: {total_mul}")
 
+def part_two():
+    mem = get_reader(respect_inst=True)
+    total_mul = 0
+    for res in mem:
+        if res != None:
+            f, s = res
+            total_mul += f * s
+    print(f"Part two: {total_mul}")
+
+
 if __name__ == "__main__":
     part_one()
+    part_two()
